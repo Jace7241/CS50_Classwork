9ffe6311d98aba28a91d28d0c473e1255c8b1d24
Jace7241 <Jace7241@users.noreply.github.com>
2020-06-18 20:49:22 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
-#include <cs50.h>
-#include <math.h>
+#include <cs50.h>  
+int main(void)  
+{      
+    float change;  
+    do  
+    {  
+        change = get_float("change owed = ");  
+    } while (change < 0); 
 
-int main(void)
-{
-    int total;
-    float change;
-    do
-    {
-        change = get_float ("Change - ");
-        
-        total = (change * 100);
-    }
-    while (change <0.01);
-    
-    int quarter = total / 25;
-    int dime = (total / 25)/10;
-    int nickel = ((total /25)/10)/5;
-    int penny = (((total /25)/10)/5)/1;
-    int amount = quarter + dime + nickel + penny;
-    printf("%0i\n", amount);
-}
+    int coins = 0;  
+    while (change >= 0.25)  
+    {  
+        coins ++;  
+        change = change - 0.25;  
+    }  
+
+    while (change >= 0.10)  
+    {  
+        coins ++;  
+        change = change - 0.10;  
+    }  
+
+    while (change >= 0.05)  
+    {  
+        coins ++;  
+        change = change - 0.05;  
+    }  
+
+    while (change >= 0.01)  
+    {  
+        coins ++;  
+        change = change - 0.01;  
+    }  
+    printf ("%i\n", coins);  
+} 
