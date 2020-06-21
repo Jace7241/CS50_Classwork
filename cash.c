503f20adf6ec7fd949b0a1c797d33c7c68939476
Jace7241 <Jace7241@users.noreply.github.com>
2020-06-21 01:59:25 +0000

    automated commit by submit50

--- cash.c
+++ cash.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
+//uses round command with the math library
 #include <math.h>
 
 int main(void)
@@ -8,29 +9,37 @@ int main(void)
     int  total;
     do
     {
-        change = get_float ("Amount owed - ");
-        total = round(change*100);
-
-    }while (change <= 0);
+        //asks user to insert number
+        change = get_float("Amount owed - ");
+        //rounds number and multiplies into whole number
+        total = round(change * 100);
+//does not allow negatives or 0
+    } 
+    while (change <= 0);
 
     int amount = 0;
-    while(total >= 25) {
+    //if the rounded total is above a certain number subtract by that number until it gets to be a smaller specific number
+    while (total >= 25)
+    {
         amount++;
         total = total - 25;
     }
 
 
-    while(total >= 10) {
+    while (total >= 10)
+    {
         amount++;
         total = total - 10;
     }
 
-    while(total >= 5) {
+    while (total >= 5)
+    {
         amount++;
         total = total - 5;
     }
 
-    while(total >= 1) {
+    while (total >= 1)
+    {
         amount++;
         total = total - 1;
     }
