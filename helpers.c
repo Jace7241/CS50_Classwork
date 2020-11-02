f8fa6450fd0e451c428a0f716c6ca457ab053f49
Jace7241 <Jace7241@users.noreply.github.com>
2020-11-02 21:10:19 +0000

    automated commit by submit50

--- helpers.c
+++ helpers.c
@@ -3,12 +3,12 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             RGBTRIPLE pixelA = image[i][j];
-            int average = round((pixelA.rgbtRed + pixelA.rgbtGreen + pixelA.rgbtBlue)/3.0);
+            int average = round((pixelA.rgbtRed + pixelA.rgbtGreen + pixelA.rgbtBlue) / 3.0);
             image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
         }
     }
@@ -18,12 +18,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             RGBTRIPLE pixelB = image[i][j];
-
+// convert images to pixel form
             int sepiaRed = round(.393 * pixelB.rgbtRed + .769 * pixelB.rgbtGreen + .189 * pixelB.rgbtBlue);
             int sepiaGreen = round(.349 * pixelB.rgbtRed + .686 * pixelB.rgbtGreen + .168 * pixelB.rgbtBlue);
             int sepiaBlue = round(.272 * pixelB.rgbtRed + .534 * pixelB.rgbtGreen + .131 * pixelB.rgbtBlue);
@@ -41,18 +41,18 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE temp[height][width];
 
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
         int Pos = 0;
-        for(int j = width - 1; j >= 0; j--, Pos++)
+        for (int j = width - 1; j >= 0; j--, Pos++)
         {
             temp[i][Pos] = image[i][j];
         }
     }
-
-    for(int i = 0; i < height; i++)
+//copy to final
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             image[i][j] = temp[i][j];
         }
@@ -61,27 +61,28 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 }
 
 // Blur image
+//slight blur
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE temp[height][width];
 
-    for(int row = 0; row < height; row++)
+    for (int row = 0; row < height; row++)
     {
-        for(int col = 0; col < width; col++)
+        for (int col = 0; col < width; col++)
         {
             int Coun = 0;
-            int rowCoords[] = { row-1, row, row+1 };
-            int colCoords[] = { col-1, col, col+1 };
+            int rowCoords[] = { row - 1, row, row + 1 };
+            int colCoords[] = { col - 1, col, col + 1 };
             float totalR = 0, totalG = 0, totalB = 0;
 
-            for(int r = 0; r < 3; r++)
+            for (int r = 0; r < 3; r++)
             {
-                for(int c = 0; c < 3; c++)
+                for (int c = 0; c < 3; c++)
                 {
                     int curRow = rowCoords[r];
                     int curCol = colCoords[c];
-
-                    if (curRow>= 0 && curRow < height && curCol >= 0 && curCol < width)
+//change pixel each tome for precision
+                    if (curRow >= 0 && curRow < height && curCol >= 0 && curCol < width)
                     {
                         RGBTRIPLE pixelC = image[curRow][curCol];
 
@@ -94,13 +95,13 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             }
             temp[row][col].rgbtRed = round(totalR / Coun);
             temp[row][col].rgbtGreen = round(totalG / Coun);
-            temp[row][col].rgbtBlue = round(totalB /Coun);
+            temp[row][col].rgbtBlue = round(totalB / Coun);
+        }
     }
-    }
-
-    for(int i = 0; i < height; i++)
+// copy fianl image direct
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j<width; j++)
+        for (int j = 0; j < width; j++)
         {
             image[i][j] = temp[i][j];
         }
