89baeab0e0762a94c231955680b039c034f1530d
Jace7241 <Jace7241@users.noreply.github.com>
2020-11-18 17:30:14 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -4,7 +4,7 @@ def main():
     while True:
         height = get_int("Positive height - ")
         width = height + 1
-        if height >= 0 and height <= 23:
+        if height >= 0 and height <= 8:
             break
 
     for i in range(1, height + 1):
